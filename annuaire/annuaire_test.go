bc3bf6ac2665f389f603b861ff79bc2044257fba
Mathieu-ai <mathieu.lievre@efrei.net>
2025-05-26 17:15:19 +0200

    😠 feat: - tests for delete and modify

--- annuaire/annuaire_test.go
+++ annuaire/annuaire_test.go
@@ -46,3 +46,37 @@ func TestRechercherContact(t *testing.T) {
 		t.Error("Contact trouvé alors qu'il ne devrait pas exister")
 	}
 }
+
+func TestSupprimerContact(t *testing.T) {
+	ann := NewAnnuaire()
+	ann.AjouterContact("Test", "Contact", "0000000000")
+
+	err := ann.SupprimerContact("Test")
+	if err != nil {
+		t.Errorf("Erreur lors de la suppression: %v", err)
+	}
+
+	if ann.NombreContacts() != 0 {
+		t.Error("Le contact n'a pas été supprimé")
+	}
+
+	err = ann.SupprimerContact("Inexistant")
+	if err == nil {
+		t.Error("Attendu une erreur pour un contact inexistant")
+	}
+}
+
+func TestModifierContact(t *testing.T) {
+	ann := NewAnnuaire()
+	ann.AjouterContact("Modif", "Test", "0000000000")
+
+	err := ann.ModifierContact("Modif", "NouveauPrenom", "1111111111")
+	if err != nil {
+		t.Errorf("Erreur lors de la modification: %v", err)
+	}
+
+	contact, _ := ann.RechercherContact("Modif")
+	if contact.Prenom != "NouveauPrenom" || contact.Telephone != "1111111111" {
+		t.Errorf("Modification échouée: %+v", contact)
+	}
+}
