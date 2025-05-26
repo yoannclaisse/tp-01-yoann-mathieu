cd54d30ec0dfcd8ad1147d66a1aebfad9580cb6d
Mathieu-ai <mathieu.lievre@efrei.net>
2025-05-26 16:22:51 +0200

    🙌 init main.go

---
+++ README.md
@@ -0,0 +1,6 @@
+# Annuaire Go - TP1
+
+## groupe
+
+- Mathieu Lievre
+- Yoann Lecocq
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Annuaire Go - Version 0.1")
+    fmt.Println("TODO: Implémenter les fonctionnalités")
+}
