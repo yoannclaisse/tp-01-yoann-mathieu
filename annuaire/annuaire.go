6762290c3cee281fea158963f76a2b80c07c8f7a
ylecocq <ylecocq.io@gmail.com>
2025-05-26 16:43:06 +0200

    feat: implement functionnality to add contact

--- annuaire/annuaire.go
+++ annuaire/annuaire.go
@@ -1,5 +1,7 @@
 package annuaire
 
+import "errors"
+
 // Structure de contact
 type Contact struct {
 	Nom       string
@@ -17,4 +19,23 @@ func NewAnnuaire() *Annuaire {
 	return &Annuaire{
 		contacts: make(map[string]Contact),
 	}
+}
+
+// Ajoute un nouveau contact à l'annuaire
+func (a *Annuaire) AjouterContact(nom, prenom, telephone string) error {
+	if nom == "" || prenom == "" || telephone == "" {
+		return errors.New("tous les champs sont requis")
+	}
+
+	if _, existe := a.contacts[nom]; existe {
+		return errors.New("un contact avec ce nom existe déjà")
+	}
+
+	a.contacts[nom] = Contact{
+		Nom:       nom,
+		Prenom:    prenom,
+		Telephone: telephone,
+	}
+
+	return nil
 }
